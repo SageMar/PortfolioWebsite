c29c4c5e826b58290293929b0a41b422b2f20e1f
SageMar <sagemar@noreply.github.com>
2024-01-29 13:17:41 -0800

    Rough outline. Got text into all files with basic php file returning contact form and a css file.

--- contact.php
+++ contact.php
@@ -1,8 +1,34 @@
 <?php
+
  echo 'Welcome'.$_GET["name"];
  echo '<p>Your email address is: '.$_GET["email"].'</p>';
- echo '<p>The message you wrote and sent is:</p>';
- echo $_GET["message"];
+ echo '<p>The message you wrote and sent is:</p>'.$_GET['message'];
+
+
+ $message = $_GET['message'];
+ $name = $_GET['name'];
+ $email = $_GET['email'];
+ // this will get the value for the "reasons" contact
+ foreach ($_GET as $reason => $value) {
+  if ('submit' != $reason) {
+   if (is_array ($value)){
+    $value = implode(', ', $value);
+
+   }
+  }
+ }
+
+
+
+ $to = "Sage Markwardt<sagemarkwardt@gmail.com>";
+ $subject = "testing";
+ $headers = "From: $name" . "\r\n Reply-to: $email";
+
+ if (mail($to, $message, $subject, $headers)){
+   echo '<p>This information has been sent to the site moderator.</p>';
+ }
+
+
 
 ?>
 
